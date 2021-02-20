3cf1461f42b5a1d14b18341254fd481dbba1ef6a
Leo <leosegol@gmail.com>
2021-02-20 16:23:45 +0200

    fixed file problem

--- Server/Network.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<iostream>
-#include<WinSock2.h>
-#include<Ws2tcpip.h>
-
-void searchVictims(SOCKET s, uint32_t* address);
-
-std::string telNet(SOCKET s, std::string& command);
-
